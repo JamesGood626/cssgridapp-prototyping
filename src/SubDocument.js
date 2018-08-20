dad8c8b5c063067eb3bb02231858b19632793f21
James <jamesgood626@gmail.com>
2018-08-20 07:49:11 -0700

    Worked out the resizing of divs on click and drag events.

--- src/SubDocument.js
+++ src/SubDocument.js
@@ -41,24 +41,13 @@ class SubDocument extends Component {
     console.log("THE STYLE.SHEET: ", this.style.sheet);
   };
 
-  shouldResizeElement = (elementBoundingBox, clientX, clientY) => {
-    // elementBoundBox's shape:
-    // bottom:443
-    // left:268
-    // right:434
-    // top:277
-    // if both clientX and clientY are within range allow a resize of height and width
-    // if e.clientX is within a range of +/- 3 of left or right, then allow resize of width
-    // if e.clientY is within a range of +/- 3 of top or bottom, then allow resize of height
-  };
-
   handleMouseDown = e => {
     console.log(window.innerWidth);
     console.log(window.innerHeight);
     // console.log("TARGET: ", e.target);
     console.log("Target bounding rect: ", e.target.getBoundingClientRect());
     const targetBoundingBox = e.target.getBoundingClientRect();
-    e.target.style.position = "absolute";
+    // e.target.style.position = "absolute";
     // console.log("MouseMove clientX: ", e.clientX);
     // console.log("MouseMove clientY: ", e.clientY);
     // Call this.shouldResizeElement and return:
@@ -80,24 +69,24 @@ class SubDocument extends Component {
   };
 
   handleMouseMove = e => {
+    console.log("the mouse is down: ", this.state.mouseDown);
     if (this.state.mouseDown) {
       console.log("MouseMove clientX: ", e.clientX);
       console.log("MouseMove clientY: ", e.clientY);
       console.log(e.target.offsetLeft);
       console.log(e.target.offsetWidth);
 
-      // Gonna Have to use some maths to maintain the divs position.
-      // percentages are the only thing that did work.
-      e.target.style.left = "90%";
-      // e.target.getBoundingClientRect.right = e.clientX;
-      // e.target.style.height = `${e.clientY}px`;
-      // e.target.style.width = `${e.clientX}px`;
+      // This will suffice for resizing...
+      e.target.style.width = e.clientX - e.target.offsetLeft + 10 + "px";
+      e.target.style.height = e.clientY - e.target.offsetTop + 10 + "px";
     }
   };
 
-  // componentDidUpdate() {
-  //   console.log("this.props.htmlElements: ", this.props.htmlElements);
-  // }
+  handleMouseOut = e => {
+    this.setState({
+      mouseDown: false
+    });
+  };
 
   renderHtmlElements = () => {
     const keys = Object.getOwnPropertyNames(this.props.htmlElements);
@@ -142,6 +131,7 @@ class SubDocument extends Component {
           onMouseDown={this.handleMouseDown}
           onMouseUp={this.handleMouseUp}
           onMouseMove={this.handleMouseMove}
+          onMouseOut={this.handleMouseOut}
         />
       </div>
     );
